c9f77f26d03f8e34f90b999f48d80d19a5ca9796
David Andersson <david.o.andersson@se.atlascopco.com>
2019-10-17 21:21:06 +0200

    Dashboard and controllers

---
+++ ServiceManagementApp/Models/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceManagementApp.Models.ViewModels
+{
+    public class DashboardViewModel
+    {
+        public List<Issues> Issues { get; set; }
+        public List<Actions> Actions { get; set; }
+        public List<Projects> Projects { get; set; }
+
+    }
+}
